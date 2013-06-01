18cf72e9f69b6a2652e861baf3784320c0149bcc
Felipe Tadeo <powerphillg5@gmail.com>
2013-05-31 22:48:33 -0700

    Started the new guini

---
+++ controllers/help.php
@@ -0,0 +1,17 @@
+<?php
+
+class help {
+
+    function __construct() {
+        echo "Inside help";
+    }
+    
+    public function other(){
+     echo "we are inside other";   
+    }
+
+     public function otherone($philip){
+     echo "we are inside other" . $philip;   
+    }
+
+}
---
+++ controllers/index.php
@@ -0,0 +1,11 @@
+<?php
+
+class Index {
+
+    function __construct() {
+       echo "we are in index"; 
+    }
+
+}
+
+?>
